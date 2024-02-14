85c772b695f9457f92db71201f3ded7658854759
Khan Faiz <khan.faiz.ayub@gmail.com>
2024-02-14 18:31:39 +0530

    initial

--- src/App.js
+++ src/App.js
@@ -1,9 +1,34 @@
-export default function App() {}
+export default function App() {
+  return (
+    <div className="app">
+      <Logo />
+      <Form />
+      <PackingList />
+      <Stats />
+    </div>
+  );
+}
 
-function Logo() {}
+function Logo() {
+  return <h1>🌴 Far Away 💼</h1>;
+}
 
-function Form() {}
+function Form() {
+  return (
+    <div className="add-form">
+      <h3>What do ypu need for ypur trip</h3>
+    </div>
+  );
+}
 
-function PackingList() {}
+function PackingList() {
+  return <div className="list">List</div>;
+}
 
-function Stats() {}
+function Stats() {
+  return (
+    <footer className="stats">
+      <em>👜 Youu have X items on your list, and you already packed X (20%)</em>
+    </footer>
+  );
+}
